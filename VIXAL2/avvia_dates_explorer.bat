ca3b4169aa603be9884fbe9f59bff4e2a6e6deea
Dragoncino <100977923+Dragoncino@users.noreply.github.com>
2023-03-20 17:11:29 +0100

    created batch

---
+++ VIXAL2/avvia_dates_explorer.bat
@@ -0,0 +1,3 @@
+..\VIXAL2\bin\Debug\Vixal2.exe "..\\Data\\FullDataSet.csv" /si 1 /ei 98 /ty 5 /pr 40 /ra 10 /it 10 /re /tc 30
+..\VIXAL2\bin\Debug\Vixal2.exe "..\\Data\\FullDataSet.csv" /si 1 /ei 98 /ty 5 /pr 40 /ra 10 /it 10 /re /tc 30 /fd 2019.01.01
+
